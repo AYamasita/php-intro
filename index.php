64b0f65fb9621844e3fcebfba06068a0c00c8998
ayamasita <alexandreyamasita@gmail.com>
2019-08-26 21:37:04 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Formulário de Inscrição</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <p>FORMULARIO PARA INSCRICAO DE COMPETIDORES</p>
+    <form action="script.php" method="post">
+        <p>Seu nome: <input type="text" name="nome"> </p>
+        <p>Sua idade: <input type="text" name="idade"> </p>
+        <p><input type="submit" value="Enviar" /></p>
+    </form>
+
+</body>
+
+</html>
